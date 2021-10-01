ecd9541880a224fab2b5d4f3bbc666d612d9e525
adity <adityapratap620@gmail.com>
2021-10-01 18:56:28 +0530

    first commit

---
+++ Dynamic Chess board/index.php
@@ -0,0 +1,28 @@
+<html>
+<head> 
+</head>
+<body class = "center"><table style="border-spacing:0px;border:1px solid black">
+
+    <h1>Dynamic chess board</h1>
+<?php
+$value=$_GET['num_of_boxes'];
+for ($r=1;$r<=$value;$r++)
+{
+    echo "<tr>";
+    for ($c=1;$c<=$value;$c++){
+        $a=$r+$c;
+        if ($a%2==0){
+            echo"<td style='background-color:black;border:1px solid black; width:60px;height:60px'></td>";
+            }
+        else{
+            echo"<td></td>";
+            }
+    }
+    echo "</tr>";
+
+}
+?>
+</table>
+
+</body>
+</html>
